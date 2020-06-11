9b9bf656e479c5a8aba98200cf91d18776f60cd5
redlac <runeman2@gmail.com>
2020-06-11 16:45:22 -0600

    Added envirnoment variable config

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-var http = require('http');
-var nodemailer = require('nodemailer');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
-}).listen(8080);
-
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: '',
-        pass: ''
-    }
-})
-
-let mailOptions = {
-    from: '',
-    to: '',
-    subject: "Latest 'Ask HN: Who's Hiring?' Post",
-    text: link
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-do
-transporter.sendMail(mailOptions, (error, info){
-    if (error){
-        console.log(error)
-    } else{
-        console.log('Email sent:' + info.response);
-    }
+var nodemailer = require("nodemailer");
+
+const app = require("http").createServer((req, res) => res.send("Hacker News Jobs Emailer Sender Running!!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
-let link;
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.FROM_PASSWORD,
+  },
+});
+
+let mailOptions = {
+  from: process.env.FROM_EMAIL,
+  to: process.env.FROM_EMAIL,
+  subject: "Latest 'Ask HN: Who's Hiring?' Post",
+  text: "test",
+};
+
+sendMailInterval = () => {
+  console.log("sending mail...");
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent:" + info.response);
+    }
+  });
+};
+
+setInterval(sendMailInterval, 5000);
