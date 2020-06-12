c59b5677ceb22a2764b19237824dcf5bd2bc7180
redlac <runeman2@gmail.com>
2020-06-11 19:52:45 -0600

    Add URL filtering

--- app.js
+++ app.js
@@ -2,15 +2,54 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-var nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+const fetch = require('node-fetch');
 
-const app = require("http").createServer((req, res) => res.send("Hacker News Jobs Emailer Sender Running!!"));
 
-const PORT = process.env.PORT || 3000;
+const app = require("http").createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("Hacker-News-Jobs-Emailer-Sender-Running!!");
+});
+
+const PORT = process.env.PORT || 8080;
+const ASK_HN_URL = 'https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty';
+const HN_ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/.json?print=pretty';
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+let pollHackerNews = async () => {
+    //final list of HN urls to send by email
+
+    //Get up to 200 of the latest Ask HN stories
+    const response = await fetch(ASK_HN_URL);
+    const json = await response.json();
+
+    let idLocation = HN_ITEM_URL.indexOf('.json');
+    const regex = /.json/gi;
+
+    //insert the item id from every fetched story into the item url
+    let itemURLs = json.map( item => {
+        return HN_ITEM_URL.replace(regex, item + '.json');
+    }); 
+
+    //filter the urls for 'Ask HN' text
+    const finalURLs = await itemURLs.map(async (item) => {
+
+        const response = await fetch(item);
+        const json = await response.json();
+
+        let storyTitle = json.title;
+        if (storyTitle.includes('Ask HN'/*: Who is hiring?*/)){
+            let storyID = json.id;
+            return 'https://news.ycombinator.com/item?id=' + storyID + '\n';
+        }
+    });
+
+    //remove undefined urls from final list
+    return (await Promise.all(finalURLs)).filter(url => url !== undefined).toString();
+};
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -24,10 +63,14 @@ let mailOptions = {
   from: process.env.FROM_EMAIL,
   to: process.env.FROM_EMAIL,
   subject: "Latest 'Ask HN: Who's Hiring?' Post",
-  text: "test",
 };
 
-sendMailInterval = () => {
+sendMailInterval = async () => {
+
+  let finalURLs = await pollHackerNews();
+  console.log("finalURLs done", finalURLs);
+  mailOptions.text = finalURLs;
+  console.log(mailOptions.text);
   console.log("sending mail...");
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -38,4 +81,4 @@ sendMailInterval = () => {
   });
 };
 
-setInterval(sendMailInterval, 84000000);
+setInterval(sendMailInterval, 15000);
